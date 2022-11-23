1505ee9178871123b43fd0084837756a8e9c0b59
jumathekauka <48533528+jumathekauka@users.noreply.github.com>
2022-11-23 20:27:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const app = require("./app");
-
-app.listen(3000, () => {
-  console.log("Started express server at port 3000");
+const app = require('./app');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Started express sever on port ${PORT}`);
 });
